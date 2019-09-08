a48502d1d9258eb164e747c0ef50a6d300ffd771
josh oji <selmlocke@gmail.com>
2019-09-08 19:26:25 -0400

    second commit

---
+++ New Unity Project/Assets/HeroScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int health;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            if (Input.GetKey(KeyCode.UpArrow)) ;
+            {
+                transform.Translate(Vector2.up);
+            }
+            if (Input.GetKey(KeyCode.DownArrow)) ;
+            {
+                transform.Translate(Vector2.down);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)) ;
+            {
+                transform.Translate(Vector2.left);
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) ;
+            {
+                transform.Translate(Vector2.right);
+            }
+
+        }
+
+    }
+}
---
+++ New Unity Project/Assets/Scene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
